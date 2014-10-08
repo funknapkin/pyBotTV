d33417c3d1057ff004f841d1a5a327346cc8b774
Simon Grenier <funknapkin@gmail.com>
2014-10-08 02:15:53 -0400

    Fix code according to pep8 guidelines.

--- lib/config.py
+++ lib/config.py
@@ -1,13 +1,14 @@
 import yaml
 
+
 def config(yaml_path):
-  yaml_file = open(yaml_path, 'r')
-  try:
-    data = yaml.load(yaml_file, yaml.Loader)
-  except AttributeError:
-    data = yaml.load(yaml_file, yaml.CLoader)
-  return data
+    yaml_file = open(yaml_path, 'r')
+    try:
+        data = yaml.load(yaml_file, yaml.Loader)
+    except AttributeError:
+        data = yaml.load(yaml_file, yaml.CLoader)
+    return data
 
 if __name__ == '__main__':
-  c = config('pyBotTV/config.yaml')
-  print(c)
+    c = config('pyBotTV/config.yaml')
+    print(c)
